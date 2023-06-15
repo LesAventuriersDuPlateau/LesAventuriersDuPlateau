3fdbd8dcbe5b5f42c9fce32938f8d116b85d42a2
Samuel-Quenouille <quenouille_samuel@yahoo.com>
2023-06-15 10:09:50 +0200

    ajout des validates dans les model users et events

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ApplicationRecord
 
   has_one :cart
+
+  validates :email, 
+    presence: true,
+    format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "Entrez un email valide" }
+  
   
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-        end
+end
