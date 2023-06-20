89934e6e139c83f2da141c1fedc6c96edc19ff69
Samuel-Quenouille <quenouille_samuel@yahoo.com>
2023-06-20 18:02:21 +0200

    add localhost:3000 in development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,6 +62,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
 
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
