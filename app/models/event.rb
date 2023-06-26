595f9c47ac087e7fc084c83f9e374f228a599b94
MDFLAMorANGELS <valentinmd78@gmail.com>
2023-06-26 14:50:27 +0200

    espace sur la city

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,6 +6,6 @@ class Event < ApplicationRecord
     validates :price, numericality: { greater_than_or_equal_to: 0 }
     validates :title, length: { maximum: 30 }, format: { with: /\A[a-zA-Z\s]+\z/, message: "ne peut contenir que des lettres et des espaces" }
     validates :description, length: { maximum: 250 }, format: { with: /\A[a-zA-Z\s]+\z/, message: "ne peut contenir que des lettres et des espaces" }
-    validates :city, length: { maximum: 30 }, format: { with: /\A[a-zA-Z]+\z/, message: "ne peut contenir que des lettres" }
+    validates :city, length: { maximum: 30 }, format: { with: /\A[a-zA-Z\s]+\z/, message: "ne peut contenir que des lettres et des espaces" }
     validates :duration, numericality: { greater_than: 0 }
 end
