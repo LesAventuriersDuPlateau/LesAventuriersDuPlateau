010f54dc1479819f705c2826b706255c71c229f1
MDFLAMorANGELS <valentinmd78@gmail.com>
2023-06-26 14:39:07 +0200

    ajout d espace titre plus description

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,8 +4,8 @@ class Event < ApplicationRecord
 
     validates :title, :description, :price, :city, :zip_code, :start_date, :duration, presence: true
     validates :price, numericality: { greater_than_or_equal_to: 0 }
-    validates :title, length: { maximum: 30 }, format: { with: /\A[a-zA-Z]+\z/, message: "ne peut contenir que des lettres" }
-    validates :description, length: { maximum: 250 }, format: { with: /\A[a-zA-Z]+\z/, message: "ne peut contenir que des lettres" }
+    validates :title, length: { maximum: 30 }, format: { with: /\A[a-zA-Z\s]+\z/, message: "ne peut contenir que des lettres et des espaces" }
+    validates :description, length: { maximum: 250 }, format: { with: /\A[a-zA-Z\s]+\z/, message: "ne peut contenir que des lettres et des espaces" }
     validates :city, length: { maximum: 30 }, format: { with: /\A[a-zA-Z]+\z/, message: "ne peut contenir que des lettres" }
     validates :duration, numericality: { greater_than: 0 }
 end
